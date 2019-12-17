6012417fd7f7b3c6ef9f6fde1b881786e17520d8
fuheideMayuyu <xiaguangding@163.com>
2019-12-17 20:10:17 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <Cascader/>
+  </div>
+</template>
+
+<script>
+import Cascader from './components/Cascader.vue';
+
+export default {
+  name: 'app',
+  components: {
+    Cascader,
+  },
+};
+</script>
+
+<style lang="stylus">
+</style>
+
