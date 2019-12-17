b710640a3256b602ca98e910ee6c2407aa0e7d1a
fuheideMayuyu <xiaguangding@163.com>
2019-12-17 20:57:18 +0800

    自定义指令

--- src/components/Cascader.vue
+++ src/components/Cascader.vue
@@ -1,19 +1,32 @@
 <template>
-  <div class="Cascader">
-    <div class="title">
+  <div class="Cascader" v-click-outside="close">
+    <div class="title" @click="toggle">
       title
     </div>
-    <div class="content">
+    <div class="content" v-if="isVisible">
       content
     </div>
   </div>
 </template>
 
 <script>
+// 自定义指令 click-outside
 export default {
+  directives: { // 指令是一个方法，也有自己的生命周期
+    clickOutside: {
+      inserted (el, bindings, vnode){ // el当前dom元素 bindings当前绑定元素
+        document.addEventListener('click', function(e){
+          if(e.target === el || el.contains(e.target)){
+            return 
+          }
+          bindings.value(); // close事件
+        })
+      }
+    }
+  },
   data() {
     return {
-
+      isVisible: false
     };
   },
   computed: {
@@ -29,7 +42,12 @@ export default {
 
   },
   methods: {
-
+    close(){
+      this.isVisible = false;
+    },
+    toggle(){
+      this.isVisible = !this.isVisible
+    }
   },
   components: {
 
@@ -47,15 +65,15 @@ export default {
     line-height 30px;
   .content
     display flex
-  // .content-left
-  //   border:1px solid #ccc;
-  //   min-height:150px;
-  //   max-height:150px;
-  //   overflow-y scroll;
-  // .label
-  //   width 80px;
-  //   padding-left 5px;
-  // .label:hover
-  //   background:#999;
-  //   cursor pointer;
+  .content-left
+    border:1px solid #ccc;
+    min-height:150px;
+    max-height:150px;
+    overflow-y scroll;
+  .label
+    width 80px;
+    padding-left 5px;
+  .label:hover
+    background:#999;
+    cursor pointer;
 </style>
